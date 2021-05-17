ec7330bd05354b04bde76ee2ad0070fd04ffef3c
EshginKazimov <82080797+EshginKazimov@users.noreply.github.com>
2021-05-17 22:26:41 +0400

    Add files via upload

---
+++ FrontToBack/FrontToBack/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FrontToBack.DataAccessLayer;
+using FrontToBack.ViewModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace FrontToBack.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly AppDbContext _dbContext;
+
+        public HomeController(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            var slider = _dbContext.Slider.FirstOrDefault();
+            var sliderImages = _dbContext.SliderImages.ToList();
+            var categories = _dbContext.Categories.ToList();
+            //var products = _dbContext.Products.Include(x => x.Category).ToList();
+            var about = _dbContext.About.FirstOrDefault();
+            var aboutPolicies = _dbContext.AboutPolicies.ToList();
+
+            var homeViewModel = new HomeViewModel
+            {
+                Slider = slider,
+                SliderImages = sliderImages,
+                Categories = categories,
+                //Products = products,
+                About = about,
+                AboutPolicies = aboutPolicies
+            };
+
+            return View(homeViewModel);
+        }
+    }
+}
